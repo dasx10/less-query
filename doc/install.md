a4bc80f839a6d669d8c054a9ffedce00f612bf7b
dasx10 <dasx10@gmail.com>
2021-06-15 18:40:38 +0300

    docs

---
+++ doc/install.md
@@ -0,0 +1,14 @@
+---
+## Install
+```
+npm install -D less-query
+```
+
+## Use
+set the entry point to your file
+
+```
+@import './node_modules/less-query/style.less';
+```
+
+---
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const gcmq = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-clean-css');
+const fileinclude = require('gulp-file-include');
 
 function css(){
     return gulp.src('./test/src/style.spec.less')
@@ -11,9 +12,16 @@ function css(){
     .pipe(gulp.dest('./test/dist'))
 }
 
+function readme() {
+	return gulp.src('./doc/README.md')
+	.pipe(fileinclude())
+	.pipe(gulp.dest('./'));
+}
+
 function watch(){
     gulp.watch(['./test/src/style.spec.less'],css);
 }
 
 module.exports.css = css;
+module.exports.readme = readme;
 module.exports.watch = watch;
